11786d5606ac2bdcc0490fb669ddcdff6d31a759
Jeff Tan <jefftan969@gmail.com>
2023-01-20 01:46:14 -0500

    update run script

--- torch-ngp/run.sh
+++ torch-ngp/run.sh
@@ -1,2 +1,3 @@
 #python3 main_nerf.py data/fox --workspace trial_nerf --fp16 --tcnn --cuda_ray --preload
-python3 main_nerf.py data/box --workspace box_nerf --fp16 --tcnn --cuda_ray
+#python3 main_nerf.py data/box --workspace box_nerf --fp16 --tcnn --cuda_ray
+python3 main_nerf.py data/drone_mall --workspace drone_mall_nerf --fp16 --tcnn --cuda_ray
--- torch-ngp/run_gui.sh
+++ torch-ngp/run_gui.sh
@@ -1,1 +1,3 @@
-python3 main_nerf.py data/fox --workspace trial_nerf --fp16 --tcnn --cuda_ray --preload --gui
+#python3 main_nerf.py data/fox --workspace trial_nerf --fp16 --tcnn --cuda_ray --preload --gui
+#python3 main_nerf.py data/box --workspace box_nerf --fp16 --tcnn --cuda_ray --gui
+python3 main_nerf.py data/drone_mall --workspace drone_mall_nerf --fp16 --tcnn --cuda_ray --gui
