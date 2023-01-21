0b3fa991d9f61ac9d814fd54dcaad939c8636150
Jeff Tan <jefftan969@gmail.com>
2023-01-21 18:34:46 -0500

    torch-ngp runfiles

--- torch-ngp/run.sh
+++ torch-ngp/run.sh
@@ -1,3 +1,8 @@
-#python3 main_nerf.py data/fox --workspace trial_nerf --fp16 --tcnn --cuda_ray --preload
-#python3 main_nerf.py data/box --workspace box_nerf --fp16 --tcnn --cuda_ray
-python3 main_nerf.py data/drone_mall --workspace drone_mall_nerf --fp16 --tcnn --cuda_ray
+#python3 main_nerf.py data/fox --workspace fox_nerf --fp16 --cuda_ray --preload --bound 4
+#python3 main_nerf.py data/box --workspace box_nerf --fp16 --tcnn --cuda_ray --bound 2
+#python3 main_nerf.py data/drone_mall --workspace drone_mall_nerf --fp16 --tcnn --cuda_ray
+#python3 main_nerf.py data/doherty --workspace doherty_nerf --fp16 --tcnn --cuda_ray
+#python3 main_nerf.py data/wean --workspace wean_nerf --fp16 --tcnn --cuda_ray
+#python3 main_nerf.py data/hamerschlag --workspace hamerschlag_nerf --fp16 --cuda_ray
+python3 main_nerf.py data/gates --workspace gates_nerf --fp16 --tcnn --cuda_ray
+#python3 main_nerf.py data/jefftantheman --workspace jeff_nerf --fp16 --tcnn --cuda_ray
--- torch-ngp/run_gui.sh
+++ torch-ngp/run_gui.sh
@@ -1,3 +1,7 @@
-#python3 main_nerf.py data/fox --workspace trial_nerf --fp16 --tcnn --cuda_ray --preload --gui
+#python3 main_nerf.py data/fox --workspace fox_nerf --fp16 --cuda_ray --preload --gui --bound 4
 #python3 main_nerf.py data/box --workspace box_nerf --fp16 --tcnn --cuda_ray --gui
-python3 main_nerf.py data/drone_mall --workspace drone_mall_nerf --fp16 --tcnn --cuda_ray --gui
+#python3 main_nerf.py data/drone_mall --workspace drone_mall_nerf --fp16 --tcnn --cuda_ray --gui
+#python3 main_nerf.py data/doherty --workspace doherty_nerf --fp16 --tcnn --cuda_ray --gui
+#python3 main_nerf.py data/wean --workspace wean_nerf --fp16 --tcnn --cuda_ray --gui
+python3 main_nerf.py data/hamerschlag --workspace hamerschlag_nerf --fp16 --cuda_ray --bound 2 --gui
+#python3 main_nerf.py data/gates --workspace gates_nerf --fp16 --tcnn --cuda_ray --gui
